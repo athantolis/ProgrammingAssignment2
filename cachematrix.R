0e88b83145f688dbfca3e867b394b5fd57c15a77
athantolis <athan_tolis@yahoo.com>
2015-11-20 15:25:40 +0000

    Two Functions for Assignment 2
    These are slightly tweaked versions of the instructor's functions to
    compute a vector's mean

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Two functions here work in tandem to invert an invertible matrix
 
-## Write a short comment describing this function
+## Writes up a list that covers both cases
+## namely when the inverse is cached and when it's not
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	inv <- NULL
+	set <- function(y) {
+		x <<- y
+		inv <<- NULL
+	}
+	get <- function() x
+	setinverse <- function(inverse) inv <<- inverse
+	getinverse <- function() inv
+	list(set = set, get = get,
+		setinverse = setinverse,
+		getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Returns a matrix that is the inverse of x
+## after first taking care to avoid the computation if unnecessary
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	inv <- x$getinverse()
+	if(!is.null(inv)) {
+		message("getting cashed data")
+		return(inv)
+	}
+	data <-x$get()
+	inv<- solve(data, ...)
+	x$setinverse(inv)
+	inv
 }
